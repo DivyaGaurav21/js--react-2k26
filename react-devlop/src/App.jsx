82d9943b216fe6ed6065a61e29dd08e33460076a
divya.gaurav <divya.gaurav@ipsator.com>
2026-02-07 16:23:37 +0530

    first commit: setup done

---
+++ react-devlop/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import FormValidation from './components/question/FormValidation'
+
+function App() {
+
+  return (
+    <>
+      <FormValidation />
+    </>
+  )
+}
+
+export default App
---
+++ react-devlop/src/components/question/FormValidation.jsx
@@ -0,0 +1,11 @@
+//------- create a form with validation and error handling----------------//
+
+import React from 'react'
+
+const FormValidation = () => {
+  return (
+    <div>FormValidation</div>
+  )
+}
+
+export default FormValidation
