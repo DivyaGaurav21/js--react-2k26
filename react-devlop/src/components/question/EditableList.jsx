a93bc76c6cc8f02633863f655760feb8cff9c3a6
divya.gaurav <divya.gaurav@ipsator.com>
2026-02-20 03:09:00 +0530

    machine coding inline editable list

--- react-devlop/src/components/question/EditableList.jsx
+++ react-devlop/src/components/question/EditableList.jsx
@@ -1,44 +1,76 @@
-//create inline Editable list
-import React, {useState} from "react";
+//CREATE INLINE EDITABLE LIST
 
+import React, { useState } from "react";
 
-const todoList = [
-    { id: 1, title: "Eat Meal" },
-    { id: 2, title: "play football" },
-    { id: 3, title: "Read books" },
-]
 
 const EditableList = () => {
+    const [todos, setTodos] = useState([
+        { id: 1, title: "Eat Meal" },
+        { id: 2, title: "Play Football" },
+        { id: 3, title: "Read Books" },
+    ]);
+    const [inputVal, setInputVal] = useState("");
     const [editId, setEditId] = useState(null);
-    const [value, setValue] = useState("");
 
     const editHandler = (todo) => {
-          setEditId(todo.id);
+        setEditId(todo.id)
+        setInputVal(todo.title)
     }
 
-    return (
+    const updateHandler = (Id) => {
+         if (!inputVal.trim()) return;
+        const updatedArr = todos.map(todo => {
+            if (todo.id == Id) {
+                return { ...todo, title: inputVal }
+            } else {
+                return todo;
+            }
+        })
+        setTodos(updatedArr);
+        setEditId(null);
+    }
+
+    const cancelHandler = () => {
+        setInputVal("");
+        setEditId(null)
+    }
+
+    const deleteHandler = (Id) => {
+        setTodos(todos.filter(todo => todo.id != Id));
+    }
+
+    return <section>
         <div className="flex flex-col gap-2">
-            {todoList.map(todo => (
-                <div key={todo.id} className="flex flex-row justify-between items-center border-2 border-black p-2 relative">
-                    <p>{todo.title}</p>
+            {todos.map((todo => (
+                <div key={todo.id} className="flex flex-row justify-between items-center p-2 border border-black">
+                    {todo.id === editId ?
+                        <input
+                            type="text"
+                            value={inputVal}
+                            className="border border-black p-1"
+                            onChange={(e) => setInputVal(e.target.value)}
+                            autoFocus
+                        />
+                        : <p>{todo.title}</p>
+                    }
                     <div className="flex flex-row gap-1">
-                        {editId ? 
-                        <div>
-                            <input
-                              type="text"
-                              className="border border-black absolute left-2 top-3"
-                              value={value}
-                              onChange={(e) => setValue(e.target.value)}
-                            />
-                        </div> :
-                        <button className="btn" onClick={() => editHandler(todo)}>Edit</button>
+                        {todo.id === editId ?
+                            <>
+                                <button className="btn" onClick={() => updateHandler(todo.id)}>Update</button>
+                                <button className="btn" onClick={cancelHandler}>Cancel</button>
+                            </>
+                            :
+                            <>
+                                <button className="btn" onClick={() => editHandler(todo)}>Edit</button>
+                                <button className="btn"onClick={() => deleteHandler(todo.id)}>Delete</button>
+                            </>
                         }
-                        <button className="btn">Delete</button>
                     </div>
                 </div>
-            ))}
+            )))}
+
         </div>
-    );
+    </section>;
 };
 
 export default EditableList;
